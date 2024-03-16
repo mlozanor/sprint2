d3534fb260bd47550f63c1c7aba2fed713285bfa
marianalozano <m.lozanor2@uniandes.edu.co>
2024-03-16 14:43:09 -0500

    posottt

--- monitoring/Documentos/logic/Documentos_logic.py
+++ monitoring/Documentos/logic/Documentos_logic.py
@@ -5,3 +5,13 @@ def get_all_documentos():
 
 def get_documentos(documentos_pk):
     return Documentos.objects.get(pk=documentos_pk)
+
+def create_documentos(documentos):
+    return Documentos.objects.create(
+        nombre=documentos['nombre'],
+        descripcion=documentos['descripcion'],
+        fecha_creacion=documentos['fecha_creacion'],
+        fecha_modificacion=documentos['fecha_modificacion'],
+        usuario_creacion=documentos['usuario_creacion'],
+        usuario_modificacion=documentos['usuario_modificacion']
+    )
